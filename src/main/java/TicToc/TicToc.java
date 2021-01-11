a427772ed1f1d6099caabb818a864af07dbe8443
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-11 09:38:26 +0530

    Added code for creating board

--- src/main/java/TicToc/TicToc.java
+++ src/main/java/TicToc/TicToc.java
@@ -6,6 +6,18 @@ public class TicToc {
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to tictoc game");
+		TicToc tictoc=new TicToc();
+		tictoc.createBoard();
 	
 	}
+	
+	public char[] createBoard(){
+		char[]  board=new char[10];
+		for(int i=1;i<=10;i++)
+		{
+			board[i]=' ';
+		}
+	return board;
 	}
+
+}
