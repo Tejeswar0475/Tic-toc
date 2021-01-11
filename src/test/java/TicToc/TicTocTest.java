122038ca205e1bfc2b0e17c230638d845e4736bf
Tejeswar0475 <tejeswar096@gmail.com>
2021-01-11 09:35:02 +0530

    Added welcome message

---
+++ src/test/java/TicToc/TicTocTest.java
@@ -0,0 +1,15 @@
+package TicToc;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TicTocTest
+{
+	@Test
+	public void gameTest()
+	{
+		TicTocTest tictactoe=new TicTocTest();
+
+	}
+
+}
